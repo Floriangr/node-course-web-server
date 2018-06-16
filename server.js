55304fde9cb7a04cdbe3adb7c13dec4abb089982
Flo <florian.gruenwald@eon.com>
2018-06-16 11:32:47 +0200

    bugfix

--- server.js
+++ server.js
@@ -23,11 +23,6 @@ app.use((req, res, next) => {
   next()
 })
 
-app.use((req, res, next) => {
-  res.render('maintenance.hbs')
-  next()
-})
-
 app.use(express.static(__dirname + "/public"))
 
 hbs.registerHelper('getCurrentYear', () => {
